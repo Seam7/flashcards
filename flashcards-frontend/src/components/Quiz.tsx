6d92181f7559d10bfefac741935a4327f7a79b60
Samuel Alvarez <seam.2194@gmail.com>
2025-10-18 12:36:07 -0500

    Add quiz card behavior and css animation

--- flashcards-frontend/src/components/Quiz.tsx
+++ flashcards-frontend/src/components/Quiz.tsx
@@ -1,3 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+import { getSingleDeck } from "../queries/getDecks";
+import type { Flashcard } from "../../types";
+import { useState } from "react";
+
+// !TODO: Shuffle the deck before starting the quiz
 export const Quiz = () => {
-  return <div>Quiz</div>;
+  const { id } = useParams();
+  const { data } = useQuery({
+    queryKey: ["quiz", id],
+    queryFn: () => getSingleDeck(Number(id)),
+  });
+  console.log({ data });
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#79CCBF",
+        perspective: "1000px",
+        margin: "-30px",
+      }}
+    >
+      <QuizCard card={data?.cards[0] as Flashcard} />
+    </div>
+  );
+};
+
+const QuizCard = ({ card }: { card: Flashcard }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+  return (
+    <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={handleFlip}>
+      <div className="card-front">
+        <div className="card-front-content">
+          <h1>{card?.question || "Question"}</h1>
+        </div>
+      </div>
+      <div className="card-back">
+        <div className="card-back-content">
+          <h1>{card?.answer || "Answer"}</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
